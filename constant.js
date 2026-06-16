0230acadc9e8eafea0fb01f6a74435ec2fa0a8ec
Canace Chen <c.chen0320213@arts.ac.uk>
2026-06-16 10:40:02 +0100

    fixed loading css delay

--- constant.js
+++ constant.js
@@ -1,14 +1,13 @@
 // Create a function to generate head content for html
-function printHTMLhead(){
-    let text = `
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=0.7">
-<link rel="preconnect" href="https://unpkg.com">
-<link rel="stylesheet" href="style.css"> <!--link to css-->    
-<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-<meta name="author" content="Canace Chen">
-    `;
-    document.head.innerHTML = text;
-}
+// function printHTMLhead(){
+//     let text = `
+// <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=0.7">
+// <link rel="stylesheet" href="style.css"> <!--link to css-->    
+// <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+// <meta name="author" content="Canace Chen">
+//     `;
+//     document.head.innerHTML = text;
+// }
 
 // Create a function to generate top navigation content for home
 function printHTMLtopnav(){
